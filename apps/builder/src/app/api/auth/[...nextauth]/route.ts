e6b8e24a23eb567ba0a9ee4fdbb26e0f0c22b542
Lukaaaaah7 <Lukapontes2007@gmail.com>
2025-04-17 17:39:54 -0300

    Update route.ts

--- apps/builder/src/app/api/auth/[...nextauth]/route.ts
+++ apps/builder/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,20 @@
-import { NextResponse } from "next/server";
+import { authHandlers } from "@/features/auth/lib/nextAuth";
+import { env } from "@typebot.io/env";
+import { mockedUser } from "@typebot.io/lib/mockedUser";
+import { type NextRequest, NextResponse } from "next/server";
 
-export const GET = () => {
-  return NextResponse.json({ message: "Auth bypassed (GET)" }, { status: 200 });
+export const GET = (req: NextRequest) => {
+  const isMockingSession =
+    req.url.endsWith("/api/auth/session") && env.NEXT_PUBLIC_E2E_TEST;
+  if (isMockingSession) return NextResponse.json({ user: mockedUser });
+  return authHandlers.GET(req);
 };
 
-export const POST = () => {
-  return NextResponse.json({ message: "Auth bypassed (POST)" }, { status: 200 });
+export const POST = (req: NextRequest) => {
+  return authHandlers.POST(req);
 };
 
+// Accept company firewall requests? Not sure if it is still necessary
 export const HEAD = () => {
-  return NextResponse.json({ message: "Auth bypassed (HEAD)" }, { status: 200 });
+  return NextResponse.json({ message: "ok" }, { status: 200 });
 };
